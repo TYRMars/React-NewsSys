3b7341e8cf9eb1170fcf2ae2969720fbc924e710
张佳楠 <zhangjianan1996@qq.com>
2017-05-18 20:57:24 +0800

    webpack
    实时更新问题解决

--- 05-01/src/js/index.js
+++ 05-01/src/js/index.js
@@ -2,6 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  <h1>hello world adadfd</h1>,
+  <h1>hello world ！！</h1>,
   document.getElementById('example')
 );
