a75894c9a63cc7995abc5d3fed229a2e12cab44c
张佳楠 <zhangjianan1996@qq.com>
2017-05-31 16:10:25 +0800

    08-03
    子页面与父页面传递参数，事件与数据的双向绑定

---
+++ 08-03/src/js/components/bodychild.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default class BodyChild extends React.Component{
+
+  render() {
+    return(
+      <div>
+        <p>子页面输入：<input type="text" onChange={this.props.handleChildValueChange}/></p>
+      </div>
+    )
+  }
+}
---
+++ 08-03/src/js/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class CompomentFooter extends React.Component{
+  render(){
+    return(
+      <footer>
+      <h1>这里是尾部</h1>
+      </footer>
+    );
+  }
+}
