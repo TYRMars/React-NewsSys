64ad790d6b4ed2f224617216f1a201d94549f197
张佳楠 <zhangjianan1996@qq.com>
2017-06-13 21:38:31 +0800

    update

--- 10-01/src/js/root.js
+++ 10-01/src/js/root.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './index';
 import CompomentList from './components/list';
-import {Router,Route,hashHistory} from ‘react-router’;
+import {Route,BrowserRouter,Switch} from 'react-router-dom';
 
 export default class Root extends React.Component{
   render(){
     return(
       //这里替换了之前的Index，变成了程序的入口
-      <Router history={hashHistory}>
-        <Route component={index} path="/"></Route>
-        <Route component={CompomentList} path="list"></Route>
-      </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route component={index} path="/"></Route>
+          <Route component={CompomentList} path="list"></Route>
+        </Switch>
+      </BrowserRouter>
     );
   };
 }
