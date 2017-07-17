06e305da27bc76022060e81fc97cc77606dc4516
张佳楠 <zhangjianan1996@qq.com>
2017-07-17 22:39:22 +0800

    Update

--- 13-01/src/js/components/pc_footer.js
+++ 13-01/src/js/components/pc_footer.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Row,Col} from 'antd';
+import {Row, Col} from 'antd';
 export default class PCFooter extends React.Component {
-  render(){
-    return(
-      <footer>
-        <Row>
-          <Col span={2}></Col>
-          <Col span={20}>
-            &copy;&nbsp;2017 ReactNews. All Right Reserved.
-          </Col>
-          <Col span={2}></Col>
-        </Row>
-      </footer>
-    )
-  }
+
+	render() {
+		return (
+			<footer>
+				<Row>
+					<Col span={2}></Col>
+					<Col span={20} class="footer">
+            &copy;&nbsp;2017 ReactNews. All Rights Reserved.
+					</Col>
+					<Col span={2}></Col>
+				</Row>
+			</footer>
+		);
+	};
 }
---
+++ 13-010/src/js/components/pc_header.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import {Row,Col} from 'antd';
+import {Menu,Icon,Tabs,message,Form} from 'antd';
+import {Input,Button,CheckBox,Modal} from 'antd';
+const FormItem = Form.Item;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const TabPane = Tabs.TabPane;
+
+class PCHeader extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      current:'top',
+      Visible:false,
+      action:'login',
+      hasLogined:false,
+      userNickName:'',
+      userid:0
+    };
+  };
+  //设置可视化
+  setModalVisible(value){
+    this.setState({Visible:value});
+  };
+  //
+  handleClick(e){
+    if(e.key=='register'){
+      this.setState({current:'register'});
+      this.setModalVisible(true);
+    }else{
+      {this.setState(current:e.key)}
+    }
+  };
+  //
+  handleSubmit(e){
+    //页面向API进行提交数据
+  };
+
+  showModal (){
+    this.setState({
+      visible: true,
+    });
+  }
+  handleOk (e){
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  handleCancel (e){
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render(){
+    let {getFieldDecorator} = this.props.form; //接收参数
+    const userShow = this.state.hasLogined
+    ?
+    <Menu.Item key="logout" class="register">
+        <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+        &nbsp;&nbsp;
+        <link target="_blank">
+          <Button type="dashed" htmlType="button">个人中心</Button>
+        </link>
+        &nbsp;&nbsp;
+        <Button type="ghost" htmlType="button">退出</Button>
+    </Menu.Item>
+    :
+    <Menu.Item key="register" class="register">
+      <Icon type="user" onClick={this.showModal}/>注册／登录
+    </Menu.Item>;
+    return(
+      <header>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={4}>
+            <a href="/" class="logo">
+              <img src="./src/images/news.svg" alt="logo"/>
+              <span>ReactNews</span>
+            </a>
+          </Col>
+          <Col span={16}>
+            <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
+              <Menu.Item key="top">
+                <Icon type="appstore"/>头条
+              </Menu.Item>
+              <Menu.Item key="shehui">
+                <Icon type="appstore"/>社会
+              </Menu.Item>
+              <Menu.Item key="guonei">
+                <Icon type="appstore"/>国内
+              </Menu.Item>
+              <Menu.Item key="guowai">
+                <Icon type="appstore"/>国际
+              </Menu.Item>
+              <Menu.Item key="yule">
+                <Icon type="appstore"/>娱乐
+              </Menu.Item>
+              <Menu.Item key="tiyu">
+                <Icon type="appstore"/>体育
+              </Menu.Item>
+              <Menu.Item key="keji">
+                <Icon type="appstore"/>科技
+              </Menu.Item>
+              <Menu.Item key="shishang">
+                <Icon type="appstore"/>时尚
+              </Menu.Item>
+              {userShow}
+            </Menu>
+            <Modal title="用户中心" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}  wrapClassName="vertival-center-model"  okText="关闭">
+              <Tabs type="card">
+                <TabPane>
+                <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                  <FormItem label="账户">
+                    <Input placeholder="请输入您的账号" {...getFieldDecorator('r_userName')}/>
+                  </FormItem>
+                  <FormItem label="密码">
+                    <Input type="password" placeholder="请输入您的密码" {...getFieldDecorator('r_password')}/>
+                  </FormItem>
+                  <FormItem label="确认密码">
+                    <Input type="password" placeholder="请再次输入您的密码" {...getFieldDecorator('r_confirmPassword')}/>
+                  </FormItem>
+                  <Button type="primary" htmlType="submit">注册</Button>
+                </Form>
+                </TabPane>
+              </Tabs>
+            </Modal>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+      </header>
+    );
+  };
+}
+export default PCHeader = Form.create({})(PCHeader);
