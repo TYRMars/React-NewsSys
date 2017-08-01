64ccb8fba4231d32b7ceadc8160eb10578bd75da
张佳楠 <zhangjianan1996@qq.com>
2017-08-01 23:36:41 +0800

    评论功能

--- 15-01/src/js/components/mobile_news_details.js
+++ 15-01/src/js/components/mobile_news_details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Row, Col, BackTop} from 'antd';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
+import CommonComments from './common_comments';
 export default class MobileNewsDetails extends React.Component {
   constructor() {
     super();
@@ -31,6 +32,8 @@ export default class MobileNewsDetails extends React.Component {
               <div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
             </Col>
           </Row>
+          <hr/>
+          <CommonComments/>
           <MobileFooter/>
           <BackTop/>
         </div>
--- 15-01/src/js/components/pc_news_details.js
+++ 15-01/src/js/components/pc_news_details.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Row, Col, BackTop} from 'antd';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
-import PCNewsImageBlock from './pc_news_image_block'
+import PCNewsImageBlock from './pc_news_image_block';
+import CommonComments from './common_comments';
 export default class PCNewsDetails extends React.Component {
   constructor() {
     super();
@@ -30,6 +31,8 @@ export default class PCNewsDetails extends React.Component {
           <Col span={2}></Col>
           <Col span={14} className="container">
             <div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+            <hr/>
+            <CommonComments uniquekey={this.props.params.uniquekey}/>
           </Col>
           <Col span={6}>
             <PCNewsImageBlock count={40} type="top" width="100%" cartTitle="相关新闻" imageWidth="150px"/>
